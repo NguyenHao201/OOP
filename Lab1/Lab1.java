9466aa90cf872de00b25869ec9657ee34a7e7aa1
NguyenHao201 <167590907+NguyenHao201@users.noreply.github.com>
2024-04-21 10:47:39 +0700

    version 1

---
+++ Lab1/Lab1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab1;
+
+/**
+ *
+ * @author Student
+ */
+public class Lab1 {
+
+    public static void main(String[] args) {
+        //System.out.print(new Question1(31355).SumirstLastDigit());
+        //System.out.print("Minimum number: "+ new Question2(2, 3, 2).findMinimumNumber());
+        System.out.print("HailstoneSequance: " + new Question3(5).HailstoneSequance());
+    }
+}
---
+++ Lab1/Question1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab1;
+
+/**
+ *
+ * @author Student
+ */
+public class Question1 {
+
+    
+    private int number;
+    
+    public Question1(int number) {
+        this.number = number;
+    }
+    
+    public int SumirstLastDigit()
+    {
+        int lastDigit = number%10;
+        int firstDigit = number;
+        while(firstDigit>=10)
+            firstDigit/=10;
+        return lastDigit + firstDigit;
+    }
+    
+    
+}

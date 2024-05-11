28291d34e84835fde683c8e8337dc019f05df815
NguyenHao201 <167590907+NguyenHao201@users.noreply.github.com>
2024-05-11 14:42:08 +0700

    version 4

--- Lab3/src/main/java/com/mycompany/lab3/Lab3.java
+++ Lab3/src/main/java/com/mycompany/lab3/Lab3.java
@@ -9,6 +9,8 @@ package com.mycompany.lab3;
  * @author tai.tran
  */
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 //class CD implements Comparable<CD> {
 public class Lab3 {
@@ -18,9 +20,9 @@ public class Lab3 {
         ArrayList<CD> cds = new ArrayList<>();
 //add
         cds.add(new CD("123444", "game", "audio", "conan", 2003, 100));
-        cds.add(new CD("123443", "game1", "audio1", "conan1", 20031, 1000));
-        cds.add(new CD("123442", "game2", "audio1", "conan1", 20032, 10009));
-        cds.add(new CD("123441", "game2", "audio1", "conan1", 20032, 10056));
+        cds.add(new CD("123443", "game1", "audio1", "conan1", 2005, 1000));
+        cds.add(new CD("123442", "game2", "audio1", "conan1", 2004, 10009));
+        cds.add(new CD("123441", "game2", "audio1", "conan1", 2002, 10056));
         
 //Search CD by CD title
         String checkTitle = SearchByTitle(cds, "conan");
@@ -30,11 +32,13 @@ public class Lab3 {
         // System.out.println(checkCollection);
  //Delete CD by CD ID       
         //String deletecDID = deleteCDId(cds, "123444");
-        
+ //Edit CD information by Id
         String result =  EditCD(cds,"123444", "Edited Title" );
             for(CD cd : cds){
-                System.out.println(cd);
+//                System.out.println(cd);
             }
+//       Sort the CD list ascending by year of release
+//        sortCDAscendingByYear(cds);
                     
     }
     public static String EditCD(ArrayList<CD> cds, String Id, String title){
@@ -74,6 +78,13 @@ public class Lab3 {
             }
         }
         return "NotFound";
+    }
+    
+    public static void sortCDAscendingByYear(ArrayList<CD> cds){
+         Collections.sort(cds, Comparator.comparingInt(CD::getYear));
+        for(CD cd : cds){
+                System.out.println(cd);
+            }
     }
 
 }
@@ -115,18 +126,5 @@ public class Lab3 {
 //            }
 //        };
 //
-//        public void deleteCDById(ArrayList<CD> cds ) {
-//            boolean deleted = false;
-//            for (int i = 0; i < cds.size(); i++) {
-//                if (cds.get(i).getYear() == cds.toString()) {
-//                    cds.remove(i);
-//                    deleted = true;
-//                    System.out.println("CD with ID " + cdId + " deleted successfully.");
-//                    break;
-//                }
-//            }
-//            if (!deleted) {
-//                System.out.println("CD with ID " + cdId + " not found.");
-//            }
-//        }
+
 

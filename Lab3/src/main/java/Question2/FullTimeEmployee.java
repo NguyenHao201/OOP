a69995bef7ab3a560b8ceb0c8c5787727087d2ce
NguyenHao201 <167590907+NguyenHao201@users.noreply.github.com>
2024-05-23 23:03:42 +0700

    version1

---
+++ Lab3/src/main/java/Question2/FullTimeEmployee.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Question2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Computer
+ */
+public class FullTimeEmployee extends Employee {
+
+    private float salary;
+
+    public FullTimeEmployee(float salary) {
+        this.salary = salary;
+    }
+
+    public FullTimeEmployee(float salary, String employeeID, String employeeName, int yearOfBirth, String address, String phone) {
+        super(employeeID, employeeName, yearOfBirth, address, phone);
+        this.salary = salary;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public FullTimeEmployee() {
+
+    }
+
+    @Override
+    public void input() {
+        super.input();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Salary: ");
+        salary = sc.nextFloat();
+
+    }
+
+    @Override
+    public float getPayment() {
+        return getSalary();
+    }
+
+    @Override
+    public String getInfo() {
+        return " - Employee ID: " + employeeID
+                + " - Employee name: " + employeeName
+                + " - Year of birth: " + yearOfBirth
+                + " - Address: " + address
+                + " - Phone: " + phone
+                + " - Salary: " + salary;
+
+    }
+
+}
